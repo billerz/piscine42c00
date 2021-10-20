62275ec9fe7374314332cffc4b7e8aa6f6a829ab
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2021-10-20 23:57:11 +0700

    AddNorm

--- ex00/ft_putchar_dev.c
+++ ex00/ft_putchar_dev.c
@@ -16,3 +16,9 @@ void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
+
+int	main()
+{
+	ft_putchar('c');
+	return 0;
+}
---
+++ upload_to_bitbucket.sh
@@ -0,0 +1,6 @@
+#!/bin/bash  
+git add .  
+read -p "Commit description: " desc  
+git commit -m $desc  
+git push origin master
+
