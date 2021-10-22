5f01252b2fdafc72d333605d9927153e3ab9c98a
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2021-10-22 23:15:21 +0700

    OKOK

---
+++ ex07/ft_putnbr_dev.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ppetchda <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/20 23:52:59 by porrapat          #+#    #+#             */
+/*   Updated: 2021/10/20 23:53:55 by porrapat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putnbr(nb / 10);
+		ft_putchar('8');
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr(-nb);
+	}
+	else
+	{
+		if (nb > 9)
+		{
+			ft_putnbr(nb / 10);
+		}
+		ft_putchar(48 + nb % 10);
+	}
+}
+
+int	main(void)
+{
+	ft_putnbr(1234);
+	ft_putnbr(42);
+	ft_putchar('\n');
+	ft_putnbr(4242);
+	ft_putchar('\n');
+	ft_putnbr(20000706);
+	ft_putchar('\n');
+	ft_putnbr(+2147483647);
+	ft_putchar('\n');
+	ft_putnbr(-2147483648);
+	return (0);
+}
