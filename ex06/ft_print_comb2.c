684811118325ce39ba02092638836441ade4389a
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2021-10-22 15:51:41 +0700

    EX05Completed

--- ex06/ft_print_comb2.c
+++ ex06/ft_print_comb2.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ppetchda <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/20 23:52:59 by porrapat          #+#    #+#             */
+/*   Updated: 2021/10/20 23:53:55 by porrapat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
 void ft_putchar(char c)
@@ -5,7 +17,7 @@ void ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void ft_print_comb(void)
+void ft_print_comb2(void)
 {
 	for(char a = '0'; a <= '9'; a++)
 	{
@@ -16,12 +28,12 @@ void ft_print_comb(void)
 				ft_putchar(a);
 				ft_putchar(b);
 				ft_putchar(c);
-				if(!(a == '7' && b == '8' && c == '9')) 
+				if(!(a == '7' && b == '8' && c == '9'))
 				{
 					ft_putchar(',');
 					ft_putchar(' ');
 				}
-			}			
+			}
 		}
 	}
 }
